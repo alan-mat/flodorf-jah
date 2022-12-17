f29cc48792ca258f36071aeab46a068a886af465
Alan Matykiewicz <alan.matykiewicz@outlook.com>
2022-12-17 14:41:34 +0100

    refactor json parsing into separate class

--- src/main/java/com/floridsdorf/jah/model/GameHandler.java
+++ src/main/java/com/floridsdorf/jah/model/GameHandler.java
@@ -20,42 +20,7 @@ public class GameHandler {
         prompts = new ArrayList<>();
         players = new ArrayList<>();
         currentRoundAnswers = new HashMap<>();
-        try {
-            parsePromptsJSON(promptsJSONPath);
-        }catch (IOException ioe){
-            ioe.printStackTrace();
-        }
-    }
-    
-    private void parsePromptsJSON(String path) throws IOException {
-        String text = readFileFromResources(path);
-        JSONObject jsonObject = new JSONObject(text);
-        JSONArray prompts = jsonObject.getJSONArray("prompts");
-        for(int i = 0; i < prompts.length(); i++){
-            JSONObject promptObject = prompts.getJSONObject(i);
-            //only add prompts with 1 blank
-            if(promptObject.getInt("pick") == 1)
-                this.prompts.add(promptObject.getString("text"));
-        }
-    }
-
-    private static String readFileFromResources(String fileName) throws IOException {
-        URL resource = GameHandler.class.getClassLoader().getResource(fileName);
-
-        if (resource == null)
-            throw new IllegalArgumentException("File is not found!");
-
-        StringBuilder fileContent = new StringBuilder();
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(resource.getFile())));){
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                fileContent.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return fileContent.toString();
+        prompts = JSONParser.parsePromptsJSON(promptsJSONPath);
     }
 
     public List<Player> endRound(){
